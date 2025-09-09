7b21e76af96367713cc22a9cd0d2c04e040b4f15
leo-611 <almeida.godoy@escola.pr.gov.br>
2025-09-09 09:32:04 -0300

    update

--- dom_leo.js
+++ dom_leo.js
@@ -1,9 +1,9 @@
-const rodri_circulo = document.getElementById(`circulo`)
-let tamanho_rodri = 10
+const rodri_circulo = document.getElementById('circulo');
+let tamanho_rodri = 10;
 
-rodri_circulo.addEventListener(´dblclick`,()=>{
-    rodri_circulo.style.borderRadius = `${tamanho_rodri}px`
-    rodri_circulo.style.width = `${tamanho_rodri}px`
-    rodri_circulo,style.heigth = `${tamanho_rodri}px`
-    tamanho_rodri = tamanho_rodri+20
-})
+rodri_circulo.addEventListener('dblclick', () => {
+    rodri_circulo.style.borderRadius = `${tamanho_rodri}px`;
+    rodri_circulo.style.width = `${tamanho_rodri}px`;
+    rodri_circulo.style.height = `${tamanho_rodri}px`; // Fixed typo here
+    tamanho_rodri = tamanho_rodri + 20;
+});
