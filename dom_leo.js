55829d25339706f42644c6edf18688cb2c23d8ae
leo-611 <almeida.godoy@escola.pr.gov.br>
2025-09-12 12:11:05 -0300

    update

--- dom_leo.js
+++ dom_leo.js
@@ -7,3 +7,9 @@ rodri_circulo.addEventListener('dblclick', () => {
     rodri_circulo.style.borderRadius = '50%';  // Mantém o círculo perfeito
     tamanho_rodri = tamanho_rodri + 20;  // Aumenta o tamanho por 20px
 });
+
+function mudarCor(novacor) {
+    var elemento= document.getElementById("para1")
+    elemento.style.backgroundColor=novacor;
+    elemento.style.display='inline';
+}
